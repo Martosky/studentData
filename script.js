632e34a22daedb20c86ae63f8dc85ccf01f2ddbf
Martosky <imoniaromartins@yahoo.com>
2024-11-23 23:09:09 +0100

    working oon the solution

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const selectElem = document.querySelector("#select");
 const inputValue = document.querySelector("#st-score");
 const button = document.querySelector("#btn");
+const message = document.querySelector(".output-msgs")
