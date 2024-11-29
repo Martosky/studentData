52ddf9ee33c2347de9f93b6646e64c600a661413
Martosky <imoniaromartins@yahoo.com>
2024-11-29 14:14:45 +0100

    edited the ui and did some dom accessing

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const button = document.querySelector("#btn");
+const selectContent = document.querySelector("#select-item");
+const input = document.querySelector("#no-input");
+const numberOutput = document.querySelector("#no-count");
+const subjectOutput = document.querySelector("#subjec-item");
+const studentScore = document.querySelector("#st-score");
+const studentGrade = document.querySelector("#st-grade");
+const button = document.querySelector("#btn");
 
 
 
