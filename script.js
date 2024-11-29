029e3b724d41e8a9a279d2e8e3f7dcf7dd0b8b4e
Martosky <imoniaromartins@yahoo.com>
2024-11-29 14:33:05 +0100

    programming the page now

--- script.js
+++ script.js
@@ -5,7 +5,34 @@ const numberOutput = document.querySelector("#no-count");
 const subjectOutput = document.querySelector("#subjec-item");
 const studentScore = document.querySelector("#st-score");
 const studentGrade = document.querySelector("#st-grade");
-const button = document.querySelector("#btn");
 
+const getAVerage = scores => {
+  let sum = 0
+  let average
+  for(let score of scores){
+    sum += score;
+    average = sum / scores.length
+  }
+  return average
+}
+console.log(getAVerage([100, 58, 60, 43, 38, 85, 10]))
 
+const getGrade = (score) => {
+  if(score === 100){
+    return "A+";
+  }else if(score >=90){
+    return "A";
+  }else if(score >= 80){
+    return "B";
+  }else if(score >= 70){
+    return "C";
+}else if(score >= 60){
+  return "D";
+
+}else if(score >= 50){
+  return "E";
+}else{
+  return "F"
+}
+}
 
